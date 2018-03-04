9f046261e4b83f923f4e1d86f268c2b653e430d3
seydouniar <seydouniar@gmail.com>
2018-03-04 05:10:05 +0100

    04 mars 2018

---
+++ scriptjs/etudes.js
@@ -0,0 +1,4 @@
+var text = '<div class="row"> <div class="col-sm-2">fgfvfv</div><div class="col-sm-8>'+
+'<object width="500" height="600" data="data/cv_alternance.pdf"></object></div>'+
+'<div class="col-sm-2">gtfg</div></div>';
+document.getElementById('etudes').innerHTML=text;
--- scriptjs/profil.js
+++ scriptjs/profil.js
@@ -5,13 +5,13 @@ var data  = {nom:'NIARÉ',
             telephone: '0752825307',
             info:{nationnalite:'Malienne',ville:'Bamako',sexe:'Masculin',pays:'Mali',quartier:'Banconi',
             date_naissance:'24-09-1989' },
-            desc:'',
+            desc:'Enfant du Mali.',
             url_im:''
              }
 
     var profil=
     '<div class="container">'+
-        '<div class="row "> <div class="col-sm-6 sidenav info"><h1 class = "text-primary">Information personnel</h1>'+
+        '<div class="row "> <div class="col-sm-5 sidenav info"><h1 class = "text-primary">Information personnel</h1>'+
             '<div class="row"><div class="col-sm-3">'
                +' <img class="profile-image img-thumbnail img-circle" src="https://s18.postimg.org/ceoqz5sgp/1454697_1452705681423953_7155546759707234975_n.jpg">'+
             '</div>'+
@@ -24,7 +24,24 @@ var data  = {nom:'NIARÉ',
                 '<p class="text-center">'+data.telephone+'</p><h4 class = "text-primary">Adresse:</h4>'+
                 '<p class="text-center">'+data.adresse.voie+'</p><p class="text-center">'+data.adresse.code+' '+
                 data.adresse.ville+','+data.adresse.pays+'</p></div></div></div>'+
-                '<div class = "col-sm-6" style="background-color:yellow;></div></div></div>'
+                '<div class = "col-sm-6 text-justify" id="desc">'+
+                '<h3 class = "text-primary">À propos de moi</h3>'+
+                '<p>Etudant en master <a target="_blank" href="http://www.univ-montp3.fr/miap/ens/miashs/master/index.html">MIASHS</a>'+
+                ' de <a href="http://www.univ-montp3.fr/miap/ens/miashs/master/index.html">l\'université Paùl Valery de Montpellier</a>'+
+                ', né à Bamako (Mali) le 24 Septembre 1989, j\'ai commencé mes études primaires l\'école de Korofina Nord'+
+                '(Groupe Scolaire Bahaben Santara) à l\'age de 8 ans, après avoir eu le DEF (Diplôme d\'etude fondamentale), j\'ai fait'+
+                ' le Lycée Bouillagui Fadiga de Bamako entre 2006 et 2009, obtenu le Baccalaureat Malien en 2009, j\'ai eu'+
+                ' une du gouvernement Malien pour aller étudier à l\'université <a href="http://www.univ-tiaret.dz/?lang=2" target="_blank">'+
+                'Ibn Khaldoun</a> de Tiaret en Algérie d\'où j\'ai fait la prémière année universitaire en Science et Technique de 2009 à 2010'+
+                ', Ensuite j\'ai continué <a href="https://www.univ-sba.dz/index.php?lang=fr" target="_blank" >l\'université Djillali Liabes</a> de Sidi Bel Abbès'+
+                '.</p>'+
+                '<p>Obtenu la licence en Génie informatique à l\'université Djillali Liabes en 2012, puis le Master'+
+                ' en Génie informatique en 2014, afin d\'ameliorer mes connaissances en informatique j\'ai opté pour la france'+
+                ' en 2014-2015 pour faire le master Majeur informatique à l\'université Joseph Fourirer'+
+                '(<a href="https://www.univ-grenoble-alpes.fr" target="_blank">Université Grenoble Alpes</a>) de Grenoble à'+
+                '<a href="https://im2ag.univ-grenoble-alpes.fr" target="_blank"> L\'IM2AG</a>.</p>'+
+                '<p>'+data.desc+'</p>'
+                '</div></div></div>'
                 ;
 
         
