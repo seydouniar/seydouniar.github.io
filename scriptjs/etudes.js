ff0352630b050b1e598fe3c0a666d64218d2828b
seydouniar <seydouniar@gmail.com>
2018-03-04 05:34:59 +0100

    04 mars 2018 dxf

--- scriptjs/etudes.js
+++ scriptjs/etudes.js
@@ -1,4 +1,2 @@
-var text = '<div class="row"> <div class="col-sm-2">fgfvfv</div><div class="col-sm-8>'+
-'<object width="500" height="600" data="data/cv_alternance.pdf"></object></div>'+
-'<div class="col-sm-2">gtfg</div></div>';
+var text = '<p class="text-center"><object width="800" height="500" data="data/cv_alternance.pdf"></object><p>'
 document.getElementById('etudes').innerHTML=text;
