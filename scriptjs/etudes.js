97e8b53fd8f689dd7ee0202fde1daf8cccd86776
seydouniar <seydouniar@gmail.com>
2018-11-18 14:01:02 +0100

    add catmash

--- scriptjs/etudes.js
+++ scriptjs/etudes.js
@@ -1,2 +1,4 @@
-var text = '<p class="text-center"><object width="800" height="500" data="data/cv_alternance.pdf"></object><p>'
+var text = '<div id="cvpdf"><p class="text-center">'+
+'<object width="60%" height="500" data="data/cv_alternance.pdf">'+
+'</object><p></div>';
 document.getElementById('etudes').innerHTML=text;
